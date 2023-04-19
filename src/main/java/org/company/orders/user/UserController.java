aa1a9c74ba5d6943b2e99765abed6ad33fbc3210
karakosov103 <skarakosov@leantech.ai>
2023-04-19 14:58:32 +0600

    Added Dockerfile.

--- src/main/java/org/company/orders/user/UserController.java
+++ src/main/java/org/company/orders/user/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    @GetMapping
+    @GetMapping("/api/user")
     public String randomUserName() {
         return "I'm random user!)";
     }
